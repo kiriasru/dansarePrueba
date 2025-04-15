0ec3b4e329d6be8e0b0f3ab8bdfbe9bc326a3390
Kevin Irias <140660918+kiriasru@users.noreply.github.com>
2025-04-14 19:57:21 -0600

    Update productos.js

--- assets/js/productos.js
+++ assets/js/productos.js
@@ -1,25 +1,25 @@
 export const productos = [
     {
       categoria: "Zapatillas",
-      imagen: "../assets/imgs/productos/puntas.jpg",
+      imagen: "../imgs/productos/puntas.jpg",
       titulo: "Zapatillas Elite Pointe de Grishko / Nikolay",
       precio: 450.00
     },
     {
       categoria: "Calcetines",
-      imagen: "../assets/imgs/productos/calcetines-rocha.jpg",
+      imagen: "../imgs/productos/calcetines-rocha.jpg",
       titulo: "Calcetines de pantorrilla Simone Rocha",
       precio: 620.00
     },
     {
       categoria: "Punteras",
-      imagen: "../assets/imgs/productos/punteras-gel.jpg",
+      imagen: "../imgs/productos/punteras-gel.jpg",
       titulo: "Punteras de gel de silicona para ballet",
       precio: 95.00
     },
     {
       categoria: "Leotardos",
-      imagen: "../assets/imgs/productos/leotardo-negro.jpg",
+      imagen: "../imgs/productos/leotardo-negro.jpg",
       titulo: "Black leotard Gymnastic Dance wear",
       precio: 450.00
     }
